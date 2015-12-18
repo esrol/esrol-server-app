0a377a7c0d3384087585b5c8d97dbec83bb51b96
Ivaylo Ivanov <ivchoyyyy@gmail.com>
2015-12-18 17:55:16 +0200

    added cb function when servers are ready, start implementing tests

--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,38 @@
 'use strict';
 let expect = require('chai').expect;
-let app = require('../index.js');
-let mocks = require ('./mocks/index');
+let App = require('../index.js');
+let path = require('path');
+let request = require('request');
+
 require('mocha-sinon');
 
 describe('App Success...', () => {
 
+  describe('Should create http server and use the callback', () => {
+    it('Should create an app', () => {
+      new App(path.join(__dirname, './mocks/success/http-server-1/app'), () => {
+        describe('On http request to "localhost:3333/test"', () => {
+          it('Should return "test"', (done) => {
+            request('http://localhost:3333/test', (err, response) => {
+              expect(response.body).to.equal('test');
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+
 });
 
 describe('App Fail...', () => {
+
+  let should = 'Throw an error';
+
+  describe('Creating app with wrong path', () => {
+    it(should, () => {
+      expect(() => { require('./mocks/wrong-app-path/app'); }).to.throw(Error);
+    });
+  });
 
 });
